b038cacae7bd0bbc06b3b9d2e5bc93517e480741
ViNiCiM0 <vinic.mach05@gmail.com>
2024-06-19 11:00:34 -0300

    Busca de paciente AJAX

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,27 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function () {
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
+
+    xhr.addEventListener("load", function () {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xhr.status == 200) {
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+            pacientes.forEach(paciente => {
+                adicionaPacienteNaTabela(paciente); 
+            });
+        }
+        else {
+            alert("Erro ao buscar paciente!");
+            erroAjax.classList.remove("invisivel");
+            // console.log(xhr.status);
+            // console.log(xhr.responseText);
+        }
+    });
+    xhr.send();
+
+});
